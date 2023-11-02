1f09f232a9b3a4784b05f8e1758298dc6cf1f405
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2023-11-02 12:20:36 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 ssh_port=443
 udp_port=7300
 api_token=""
-site_url=""
+api_url=""
 
 echo "start" > /var/rocket-ssh/status.txt
 
@@ -16,7 +16,7 @@ read_config(){
         ssh_port=$(grep '^ssh_port=' "$config_file" | awk -F= '{print $2}')
         udp_port=$(grep '^udp_port=' "$config_file" | awk -F= '{print $2}')
         api_token=$(grep '^api_token=' "$config_file" | awk -F= '{print $2}')
-        site_url=$(grep '^site_url=' "$config_file" | awk -F= '{print $2}')
+        api_url=$(grep '^api_url=' "$config_file" | awk -F= '{print $2}')
     fi
 }
 
@@ -27,7 +27,7 @@ config_needrestart() {
     if [ -e "$nrconf_file" ]; then
         echo '$nrconf{restart} = "a";' >> "$nrconf_file"
     fi
-    
+
 }
 
 # Function to install required packages
@@ -40,7 +40,7 @@ install_packages() {
     for package in "${packages[@]}"; do
         sudo apt-get install -y "$package"
     done
-    
+
 }
 
 
@@ -82,238 +82,6 @@ systemctl start videocall
 echo "videocall configured"
    
 }
-#!/bin/bash
-
-ssh_port=443
-udp_port=7300
-api_token=""
-site_url=""
-
-echo "start" > /var/rocket-ssh/status.txt
-
-read_config(){
-    config_file="/var/rocket-ssh/rocket_config.txt"
-
-    # Check if the config file exists
-    if [ -f "$config_file" ]; then
-        # Read the values using grep and awk
-        ssh_port=$(grep '^ssh_port=' "$config_file" | awk -F= '{print $2}')
-        udp_port=$(grep '^udp_port=' "$config_file" | awk -F= '{print $2}')
-        api_token=$(grep '^api_token=' "$config_file" | awk -F= '{print $2}')
-        site_url=$(grep '^site_url=' "$config_file" | awk -F= '{print $2}')
-    fi
-}
-
-# Function to check and update needrestart configuration
-config_needrestart() {
-    local nrconf_file="/etc/needrestart/needrestart.conf"
-    
-    if [ -e "$nrconf_file" ]; then
-        echo '$nrconf{restart} = "a";' >> "$nrconf_file"
-    fi
-    
-}
-
-# Function to install required packages
-install_packages() {
-    
-    local packages=("build-essential" "libpam0g-dev" "libcurl4-openssl-dev" "cmake" "net-tools")
-    
-    sudo apt-get update
-
-    for package in "${packages[@]}"; do
-        sudo apt-get install -y "$package"
-    done
-    
-}
-
-
-# Function to install BadVPN using a script
-setup_udpgw_service() {
-   
-    local username="videocall"
-
-    if grep -q "^$username:" /etc/passwd; then
-        echo "videocall is installed."
-    else
-        local vendorId=$(lscpu | awk -F': ' '/Vendor ID/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}')
-        if [ "$vendorId" = "ARM" ]; then
-            sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/udpgw-arm"
-        else
-            sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/udpgw-x86"
-        fi
-
-        chmod 777 /usr/bin/badvpn-udpgw
-        useradd -m videocall
-    fi
-
-    cat >  /etc/systemd/system/videocall.service << ENDOFFILE
-[Unit]
-Description=UDP forwarding for badvpn-tun2socks
-After=nss-lookup.target
-
-[Service]
-ExecStart=/usr/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:$udp_port --max-clients 999
-User=videocall
-
-[Install]
-WantedBy=multi-user.target
-ENDOFFILE
-
-systemctl enable videocall
-systemctl start videocall
-
-echo "videocall configured"
-   
-}
-
-# Function to install Nethogs using a script
-setup_nethogs() {
-    bash <(curl -Ls https://raw.githubusercontent.com/mahmoud-ap/nethogs-json/master/install.sh --ipv4)
-    wait
-}
-
-
-build_pam_file(){
- 
-    local file_url="https://raw.githubusercontent.com/magnor-ap/setup-files/main/auth.c"
-    # Define the name of the file you want to create
-    local file_path="/lib/security/rocket_ssh_auth.c"
-    # Use curl to fetch content from the URL and save it to the output file
-    curl -s -o "$file_path" "$file_url"
-
-    if [ $? -eq 0 ]; then
-
-        sed -i "s|{api_token}|$api_token|g" "$file_path"
-        sed -i "s|{site_url}|$site_url|g" "$file_path"
-
-        # Compile the updated content (assuming it's a C file)
-        compile_path="/lib/security/rocket_ssh_auth.so" 
-        
-        gcc -shared -o $compile_path -fPIC $file_path -lcurl -lpam -lpam_misc
-
-        rm $file_path
-    fi
-
-}
-
-# Function to configure PAM authentication
-config_pam_auth() {
-    
-    local rocket_ssh_auth_file="/lib/security/rocket_ssh_auth.so"
-
-    if [ -f "$rocket_ssh_auth_file" ]; then
-  
-        if ! sudo grep -q 'auth required rocket_ssh_auth.so$' /etc/pam.d/sshd; then
-            echo "auth required rocket_ssh_auth.so" | sudo tee -a /etc/pam.d/sshd
-        else
-              echo "Not it auth required"
-        fi
-        
-        if ! sudo grep -q 'session required rocket_ssh_auth.so$' /etc/pam.d/sshd; then
-            echo "session required rocket_ssh_auth.so" | sudo tee -a /etc/pam.d/sshd
-        fi
-
-    fi
-    
-    sudo systemctl restart ssh
-    sudo systemctl restart sshd
-}
-
-# Function to configure a cron job
-config_cronjob() {
-    local cron_file_path="/var/rocket-ssh/cronjob.sh"
-    
-    if [ -f "$cron_file_path" ]; then
-        chmod +x "$cron_file_path"
-        (crontab -l 2>/dev/null; echo "* * * * * $cron_file_path") | crontab -
-        sudo systemctl restart cron
-
-    fi
-}
-
-# Function to configure a ssh 
-config_sshd() {
-  
-    local rocket_sshd_file="/etc/ssh/rocket_sshd_config"
-    if [ -f "$rocket_sshd_file" ]; then
-        rm $rocket_sshd_file
-    fi
-    
-    touch $rocket_sshd_file
-
-    if ! grep -q "Include /etc/ssh/rocket_sshd_config" /etc/ssh/sshd_config; then
-        echo "Include /etc/ssh/rocket_sshd_config" | sudo tee -a /etc/ssh/sshd_config
-    fi
-
-    sudo sed -i '/^#\s*Port 22/s/^#//' /etc/ssh/sshd_config
-
-    if ! grep -qE '^\s*ClientAliveInterval' $rocket_sshd_file; then
-         echo "ClientAliveInterval 60" | sudo tee -a $rocket_sshd_file
-    fi
-
-    if ! grep -qE '^\s*ClientAliveCountMax' $rocket_sshd_file; then
-        echo "ClientAliveCountMax 2" | sudo tee -a $rocket_sshd_file
-    fi
-
-    if ! grep -qE "^\s*Port $ssh_port" $rocket_sshd_file; then
-        echo "Port $ssh_port" | sudo tee -a $rocket_sshd_file
-    fi
-
-    sudo systemctl restart ssh
-    sudo systemctl restart sshd
-
-}
-
-complete_install(){
-    echo "complete" > /var/rocket-ssh/status.txt
-
-    local conf_file_path="/var/rocket-ssh/rocket_config.txt"
-    if [ -f "$so_file_path" ]; then
-        rm $conf_file_path
-    fi
-
-    local nethogs_folder_path="/var/rocket-ssh/nethogs-json-master"
-    if [ -d "$nethogs_folder_path" ]; then
-        rm -R $nethogs_folder_path
-    fi
-
-    local coron_lock_file_path="/var/rocket-ssh/cronjob.lock"
-    if [ -f "$coron_lock_file_path" ]; then
-        sudo systemctl stop cron
-        rm $coron_lock_file_path
-        pkill -f /var/rocket-ssh/cronjob.sh 
-        sudo systemctl start cron
-    fi
-
-    local ssh_auth_file="/var/rocket-ssh/rocket_ssh_auth.so"
-    if [ -f "$ssh_auth_file" ]; then
-        rm $ssh_auth_file
-    fi
-
-    sleep 1
-    sudo systemctl restart ssh
-    sudo systemctl restart sshd
-
-    sleep 2
-    # Remove the script file
-    rm /var/rocket-ssh/install
-
-    
-}
-
-
-# Call the functions to perform the tasks
-config_needrestart
-install_packages
-read_config
-setup_nethogs
-setup_udpgw_service
-build_pam_file
-config_pam_auth
-config_cronjob
-config_sshd
-complete_install
 
 # Function to install Nethogs using a script
 setup_nethogs() {
@@ -333,7 +101,7 @@ build_pam_file(){
     if [ $? -eq 0 ]; then
 
         sed -i "s|{api_token}|$api_token|g" "$file_path"
-        sed -i "s|{site_url}|$site_url|g" "$file_path"
+        sed -i "s|{api_url}|$api_url|g" "$file_path"
 
         # Compile the updated content (assuming it's a C file)
         compile_path="/lib/security/rocket_ssh_auth.so" 
@@ -361,8 +129,8 @@ config_pam_auth() {
         if ! sudo grep -q 'session required rocket_ssh_auth.so$' /etc/pam.d/sshd; then
             echo "session required rocket_ssh_auth.so" | sudo tee -a /etc/pam.d/sshd
         fi
-
     fi
+
     sleep 1
     sudo systemctl restart ssh
     sudo systemctl restart sshd
@@ -376,7 +144,6 @@ config_cronjob() {
         chmod +x "$cron_file_path"
         (crontab -l 2>/dev/null; echo "* * * * * $cron_file_path") | crontab -
         sudo systemctl restart cron
-
     fi
 }
 
@@ -445,9 +212,11 @@ complete_install(){
     sudo systemctl restart sshd
 
     sleep 2
+
     # Remove the script file
+    rm "$0"
     rm /var/rocket-ssh/install
-    
+
 }
 
 
