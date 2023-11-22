1c8099798bc99bc6262245e427b014d97289b8a9
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2023-11-23 02:07:05 +0330

    Update rocketproc.sh

--- rocketproc.sh
+++ rocketproc.sh
@@ -2,24 +2,44 @@
 
 apiBaseUrl="{api_url}"
 apiToken="{api_token}"
+isCalcTraffic=1
+isCreateUbanner=1
+
+get_api_settings(){
+
+    local apiUrl="${apiBaseUrl}/settings?token=${apiToken}"
+    response=$(curl -s -o "$apiUrl")
+
+    if [ $? -eq 0 ]; then
+
+        isCalcTraffic=$(echo "$response" | sed -n 's/.*"servers_calc_traffic":\([^,}]*\).*/\1/p')
+        isCreateUbanner=$(echo "$response" | sed -n 's/.*"servers_create_ubanner":\([^,}]*\).*/\1/p')
+        
+    fi
+    
+    sleep 1800
+
+    get_api_settings
+}
 
 send_nethogs_to_api() {
+    if [ "$isCalcTraffic" -eq 1 ]; then
 
-    local nethogsContent=$(sudo nethogs -j -v3 -c6)
-    sudo pkill nethogs
-    
-    # Send content to API
-    if [ -n "$nethogsContent" ]; then
-        local encodedData=$(echo -n "$nethogsContent" | base64 -w 0)
+        local nethogsContent=$(sudo nethogs -j -v3 -c6)
+        sudo pkill nethogs
+        # Send content to API
+        if [ -n "$nethogsContent" ]; then
+            local encodedData=$(echo -n "$nethogsContent" | base64 -w 0)
 
-        local apiUrl="${apiBaseUrl}/traffics?token=${apiToken}"
+            local apiUrl="${apiBaseUrl}/traffics?token=${apiToken}"
 
-        jsonData="{\"data\": \"$encodedData\"}"
+            jsonData="{\"data\": \"$encodedData\"}"
 
-        curl -X POST -H "Content-Type: application/json" -d "$jsonData" "$apiUrl"
-        
-        sudo kill -9 $(pgrep nethogs)
-        sudo killall -9 nethogs
+            curl -s -o -X POST -H "Content-Type: application/json" -d "$jsonData" "$apiUrl"
+            
+            sudo kill -9 $(pgrep nethogs)
+            sudo killall -9 nethogs
+        fi
     fi
 
     sleep 5
@@ -28,35 +48,43 @@ send_nethogs_to_api() {
 }
 
 create_user_banner(){
-    local EXCLUDE_USERS=("videocall" "sshd")
 
-    for user_dir in /home/*; do
-        # Check if the item in /home is a directory
-        if [ -d "$user_dir" ]; then
-            # Extract the username from the directory path
-            username=$(basename "$user_dir")
-            # Check if the username is in the exclusion list
-            if [[ " ${EXCLUDE_USERS[*]} " != *"$username"* ]]; then
+    if [ "$isCreateUbanner" -eq 1 ]; then
 
-                local apiUrl="${apiBaseUrl}/ubanner?token=${apiToken}&username=$username"
+        local EXCLUDE_USERS=("videocall" "sshd")
 
-                local api_response=$(curl -s GET "$apiUrl")
+        for user_dir in /home/*; do
+            # Check if the item in /home is a directory
+            if [ -d "$user_dir" ]; then
+                # Extract the username from the directory path
+                username=$(basename "$user_dir")
+                # Check if the username is in the exclusion list
+                if [[ " ${EXCLUDE_USERS[*]} " != *"$username"* ]]; then
 
-                local html_file="/var/ssh-banners/${username}"
+                    local apiUrl="${apiBaseUrl}/ubanner?token=${apiToken}&username=$username"
 
-                if [ -e "$html_file" ]; then
-                   rm "$html_file"
+                    local api_response=$(curl -s -o GET "$apiUrl")
+
+                    local html_file="/var/ssh-banners/${username}"
+
+                    if [ -e "$html_file" ]; then
+                    rm "$html_file"
+                    fi
+
+                    echo "$api_response" >> "$html_file"
+                    sleep 0.2
+
                 fi
-
-                echo "$api_response" >> "$html_file"
-
-                sleep 0.5
-
             fi
-        fi
-    done
+        done
 
-    sleep 600
+    else
+        local bannerFolder="/var/ssh-banners"
+        rm "$bannerFolder"/*
+    fi
+    
+    sleep 750
+
     create_user_banner
 }
 
@@ -108,7 +136,7 @@ EOF
 
     local apiUrl="${apiBaseUrl}/resources?token=${apiToken}"
     # Send the data to the API using 'curl' (you may need to install curl if it's not already installed)
-    curl -X POST -H "Content-Type: application/json" -d "$DATA" "$apiUrl"
+    curl -s -o -X POST -H "Content-Type: application/json" -d "$DATA" "$apiUrl"
 
     sleep 120
 
@@ -120,6 +148,8 @@ reset_ssh_serivces(){
     sleep 1800
 }
 
+# call sys methods
+get_api_settings &
 send_nethogs_to_api &
 create_user_banner &
 send_system_resources &
