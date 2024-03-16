39f5f6ef596ac9e956170ddad0f27c52ff23db56
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-03-16 12:55:26 +0330

    Update ovpn-setup.sh

--- ovpn-setup.sh
+++ ovpn-setup.sh
@@ -109,6 +109,7 @@ EOF
 configure_iptable(){
     # Get primary NIC device name
     NIC=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)
+    $PROTOCOL="udp"
 echo "#!/bin/sh
 iptables -t nat -I POSTROUTING 1 -s 10.8.0.0/24 -o $NIC -j MASQUERADE
 iptables -I INPUT 1 -i tun0 -j ACCEPT
